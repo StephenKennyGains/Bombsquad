af0be65bd0002d2b9217de1d432654ab03a8e5b7
StephenKennyGains <stephenkennygains@gmail.com>
2021-10-12 15:25:50 +0000

    Add start_game function to welcome and ask players name

--- run.py
+++ run.py
@@ -1,1 +1,11 @@
 from random import *
+
+
+def start_game():
+    """
+    Asks player for their Name and used the name to prompt
+    player selection to continue or leave
+    """
+    print("\nWELCOME TO BOMBSQUAD \n")
+    player_name = input("WHAT IS THE NAME OF OUR BOMB SQUAD EXPERT?:")
+    print(f"\nOK {player_name}, ARE YOU READY TO BEGIN? \n")
