28ab15bfc14f4409b91552bd2a0d6a1be91486b8
StephenKennyGains <stephenkennygains@gmail.com>
2021-10-12 15:27:32 +0000

    Add start_or_leave function to give player option to continue or exit program

--- run.py
+++ run.py
@@ -9,3 +9,22 @@ def start_game():
     print("\nWELCOME TO BOMBSQUAD \n")
     player_name = input("WHAT IS THE NAME OF OUR BOMB SQUAD EXPERT?:")
     print(f"\nOK {player_name}, ARE YOU READY TO BEGIN? \n")
+
+
+def start_or_leave():
+    """
+    Asks player if they wish to continue and if so offer instructions
+    If player selects no, bring back to start, or re prommpt invalidity.
+    """
+    play_or_leave = input("Press -- y -- to continue, press -- n -- \
+to chicken out! \n")
+    player_entry = play_or_leave
+    player_choices = ["y", "n"]
+    if player_entry not in player_choices:
+        print("That was not an option".upper())
+        start_or_leave()
+    elif play_or_leave == ("n"):
+        print("Ok just run it again when you're ready to begin.\n".upper())
+        exit()
+    elif play_or_leave == ("y"):
+        instructions()
